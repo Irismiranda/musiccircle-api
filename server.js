e53733235365986ca637997a3d44414dd8af3965
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-11-25 02:55:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ dotenv.config()
 
 app.use(cors({
   origin: 'https://musiccircle.onrender.com', 
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
 app.use(express.json())
