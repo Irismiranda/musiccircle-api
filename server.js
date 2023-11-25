ecfdd3494102b71d044f52d3293def549c881529
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-11-25 13:26:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ const server = app.listen(4000, function(){
 const io = socketIo(server, {
   cors: {
     origin: 'https://musiccircle.onrender.com', 
-    methods: ["GET", "POST"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   },
 })
